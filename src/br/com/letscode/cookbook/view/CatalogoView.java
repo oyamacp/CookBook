34191e3e53e34aff8179a703f7bc91bebc06ce11
Cristiano Neves <cris.a.neves@gmail.com>
2022-02-17 00:54:12 -0300

    Implementação da CatalogoView utilizando o utilitário ConsoleUtils

--- src/br/com/letscode/cookbook/view/CatalogoView.java
+++ src/br/com/letscode/cookbook/view/CatalogoView.java
@@ -4,7 +4,6 @@ import br.com.letscode.cookbook.controller.Catalogo;
 import br.com.letscode.cookbook.domain.Receita;
 
 import java.util.Locale;
-import java.util.Scanner;
 
 public class CatalogoView {
     private Catalogo controller;
@@ -17,12 +16,125 @@ public class CatalogoView {
             currentIndex = 1;
             ative = controller.getReceita(currentIndex);
         } else {
-            currentIndex = -1;
+            currentIndex = 0;
             ative = null;
         }
     }
 
-    public void add() {
+    private boolean showMenu() {
+        String[] options = new String[7];
+        StringBuilder sb = new StringBuilder("#".repeat(100));
+
+        sb.append("%n").append("  + : Adicionar  %n");
+        options[0] = "+";
+
+        if (ative != null) {
+            sb.append("  E : Editar  %n");
+            options[1] = "E";
+            sb.append("  - : Remover  %n");
+            options[2] = "-";
+        }
+
+        if (controller.getTotal() > 1) {
+            sb.append("  P : Próxima  %n");
+            options[3] = "P";
+            sb.append("  A : Anterior  %n");
+            options[4] = "A";
+            sb.append("  L : Localizar  %n");
+            options[5] = "L";
+        }
+
+        sb.append("#").append(" #".repeat(48)).append("#%n");
+        sb.append("  X : Sair  %n");
+        options[6] = "X";
+        sb.append("#".repeat(100)).append("%n");
+
+        String opcao = ConsoleUtils.getUserOption(sb.toString(), options).toUpperCase(Locale.getDefault());
+        switch (opcao) {
+            case "+":
+                add();
+                break;
+            case "-":
+                del();
+                break;
+            case "P":
+                next();
+                break;
+            case "A":
+                previous();
+                break;
+            case "L":
+                find();
+                break;
+            case "X":
+                System.out.println("Obrigado!!");
+                return false;
+            default:
+                System.out.println("Opção inválida!!!");
+        }
+        return true;
+    }
+
+    private void find() {
+        //Capturar o nome da receita.
+        String name = ConsoleUtils.getUserInput("Qual o nome da receita que deseja localizar?");
+        //Procura no Catalogo a receita com o mesmo nome.
+        ative = controller.getReceita(name);
+        currentIndex = 0;
+    }
+
+    private void next() {
+        //Se estiver com uma receita ativa, ativa a próxima receita.
+        //Se NÃO estiver com uma receita ativa, ativa a primeira receita.
+        if (ative != null) currentIndex++;
+        try {
+            ative = controller.getReceita(currentIndex);
+        } catch (IllegalArgumentException e) {
+            ative = null;
+        }
+        if (ative == null) {
+            currentIndex = 1;
+            ative = controller.getReceita(currentIndex);
+        }
+    }
+
+    private void previous() {
+        //Se estiver com uma receita ativa, ativa a anterior.
+        //Se NÃO estiver com uma receita ativa, ativa a última receita.
+        if (ative != null) currentIndex--;
+        try {
+            ative = controller.getReceita(currentIndex);
+        } catch (IllegalArgumentException e) {
+            ative = null;
+        }
+        if (ative == null) {
+            currentIndex = controller.getTotal();
+            ative = controller.getReceita(currentIndex);
+        }
+    }
+
+    private void del() {
+        //Se NÃO estiver com uma receita ativa, mostra mensagem.
+        //Se estiver com uma receita ativa, confirma a operação.
+        String opcao = ConsoleUtils.getUserOption("Você deseja realmente APAGAR a receita " + ative.getNome() + "?\nS - Sim   N - Não", "S", "N");
+        //Se confirmar, solicita ao Catalogo apagar a receita.
+        if (opcao.equalsIgnoreCase("S")) {
+            controller.del(ative.getNome());
+            ative = null;
+            if (controller.getTotal() > 0) {
+                currentIndex = 0;
+                next();
+            }
+        }
+    }
+
+    private void edit() {
+        //Se NÃO estiver com uma receita ativa, mostra mensagem.
+        //Se estiver com uma receita ativa, abra a tela de edição.
+//        view();
+    }
+
+    private void add() {
         //Capturar o nome da receita.
         //Procura no Catalogo a receita com o mesmo nome.
         //Se encontrar, mostra mensagem.
@@ -31,13 +143,6 @@ public class CatalogoView {
         //Cria uma nova receita
         //Passa a receita para o Catalogo adicionar.
         //Torna a nova receita a ativa.
-//        view();
-    }
-
-    public void find() {
-        //Capturar o nome da receita.
-        //Procura no Catalogo a receita com o mesmo nome.
-//        view();
     }
 
     public void view() {
@@ -55,114 +160,5 @@ public class CatalogoView {
             System.out.println(tela);
             //Exibe o menu de opções.
         } while (showMenu());
-    }
-
-    private boolean showMenu() {
-        System.out.println("#".repeat(100));
-        System.out.println("  + : Adicionar  ");
-        if (ative != null) {
-            System.out.println("  - : Remover  ");
-        }
-        if (controller.getTotal() > 0) {
-            System.out.println("  P : Próxima  ");
-            System.out.println("  A : Anterior  ");
-            System.out.println("  L : Localizar  ");
-        }
-        System.out.println("# # # # # # # # # # # ".concat("#".repeat(78)));
-        System.out.println("  X : Sair  ");
-        System.out.println("#".repeat(100));
-
-        Scanner scanner = new Scanner(System.in);
-        String opcao = scanner.nextLine().trim().toUpperCase(Locale.getDefault());
-        switch (opcao) {
-            case "+":
-                add();
-                break;
-            case "-":
-                if (ative != null) del();
-                break;
-            case "P":
-                if (ative != null) next();
-                break;
-            case "A":
-                if (ative != null) previous();
-                break;
-            case "L":
-                if (controller.getTotal() > 0) {
-                    find();
-                }
-                break;
-            case "X":
-                System.out.println("Obrigado!!");
-//                System.exit(0);
-                return false;
-            default:
-                System.out.println("Opção inválida!!!");
-//                view();
-        }
-        return true;
-    }
-
-    public void next() {
-        //Se estiver com uma receita ativa, ativa a próxima receita.
-        //Se NÃO estiver com uma receita ativa, ativa a primeira receita.
-        if (ative != null) currentIndex++;
-        try {
-            ative = controller.getReceita(currentIndex);
-        } catch (IllegalArgumentException e) {
-            ative = null;
-        }
-        if (ative == null) {
-            currentIndex = 1;
-            ative = controller.getReceita(currentIndex);
-        }
-//        view();
-    }
-
-    public void previous() {
-        //Se estiver com uma receita ativa, ativa a anterior receita.
-        //Se NÃO estiver com uma receita ativa, ativa a última receita.
-        if (ative != null) currentIndex--;
-//        try {
-//            ative = controller.getReceita(currentIndex);
-//        } catch (IllegalArgumentException e) {
-//            ative = null;
-//        }
-//        if (ative == null) {
-//            currentIndex = controller.getTotal();
-            currentIndex = currentIndex <= 0 ? 1 : controller.getTotal();
-            ative = controller.getReceita(currentIndex);
-//        }
-//        view();
-    }
-
-    public void del() {
-        //Se NÃO estiver com uma receita ativa, mostra mensagem.
-        //Se estiver com uma receita ativa, confirma a operação.
-        //Se confirmar, solicita ao Catalogo apagar a receita.
-        System.out.println("Você deseja realmente APAGAR a receita " + ative.getNome() + "?\nS - Sim   N - Não");
-        Scanner scanner = new Scanner(System.in);
-        String opcao;
-        do {
-            opcao = scanner.nextLine().trim().toUpperCase(Locale.getDefault());
-            if (opcao.equals("S")) {
-                controller.del(ative.getNome());
-                ative = null;
-//                currentIndex = 1;
-//                next();
-                break;
-            } else if (opcao.equals("N")){
-//                view();
-                break;
-            } else {
-                System.out.println("Opção inválida!!!");
-            }
-        } while (true);
-    }
-
-    public void edit() {
-        //Se NÃO estiver com uma receita ativa, mostra mensagem.
-        //Se estiver com uma receita ativa, abra a tela de edição.
-//        view();
     }
 }
