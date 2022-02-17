1e2e1af6b247c80e45f3a318ef0f62a36244def3
Cristiano Neves <cris.a.neves@gmail.com>
2022-02-17 01:31:13 -0300

    Início da implementação de edição e inclusão das receitas

---
+++ src/br/com/letscode/cookbook/view/EditReceitaView.java
@@ -0,0 +1,15 @@
+package br.com.letscode.cookbook.view;
+
+import br.com.letscode.cookbook.domain.Receita;
+
+public class EditReceitaView {
+    private Receita receita;
+
+    public EditReceitaView(Receita receita) {
+        this.receita = receita;
+    }
+
+    public Receita edit() {
+        return null;
+    }
+}
